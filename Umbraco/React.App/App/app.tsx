97a434db5ade0cb4068f0c9e561811f26bea43b8
elgboffel <johnny2900@gmail.com>
2018-03-18 23:02:04 +0100

    Web and App Init

---
+++ Umbraco/React.App/App/app.tsx
@@ -0,0 +1,34 @@
+﻿import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import Slider from 'react-slick';
+
+module App {
+    const htmlView = document.querySelector('.react-carousel');
+
+    class ReactCarousel extends React.Component {
+        render() {
+            const settings = {
+                dots: true,
+                infinite: true,
+                speed: 500,
+                slidesToShow: 1,
+                slidesToScroll: 1
+            };
+            return (
+                <div>
+                    <h1>Hello</h1>
+                    <Slider {...settings}>
+                        <div><h3>1</h3></div>
+                        <div><h3>2</h3></div>
+                        <div><h3>3</h3></div>
+                        <div><h3>4</h3></div>
+                        <div><h3>5</h3></div>
+                        <div><h3>6</h3></div>
+                    </Slider>
+                </div>
+            );
+        }
+    }
+
+    ReactDOM.render(<ReactCarousel />, htmlView);
+}
